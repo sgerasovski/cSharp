934019e00f9c568824335aabe4d29ae201a8b0b7
sgerasovski <gerasovskis@gmail.com>
2020-03-09 20:58:31 +0100

    Initial check-in of module Homework_01

---
+++ Homework_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int inputParse;
+
+            if (int.TryParse(input, out inputParse))
+            {
+                if (inputParse == 1)
+                {
+                    Console.WriteLine("You got a new car!");
+                }
+                else if (inputParse == 2)
+                {
+                    Console.WriteLine("You got a new plane!");
+                } else if (inputParse == 3)
+                {
+                    Console.WriteLine("You got a new bike!");
+                } else
+                {
+                    Console.WriteLine("Please enter a number between 1 and 3");
+                }
+            } else
+            {
+                Console.WriteLine("Please enter a number");
+            }
+        }
+    }
+}
