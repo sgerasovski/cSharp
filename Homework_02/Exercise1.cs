4c2fce3b09ac6913e6a033c75f8bf497651a583b
sgerasovski <gerasovskis@gmail.com>
2020-03-17 23:09:25 +0100

    Homework_02

---
+++ Homework_02/Exercise1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+
+            string input = null;
+
+            int inputParse;
+
+            do
+            {
+                Console.WriteLine(input == null ? "Enter a Number" : "You entered an invaid number, please try again!");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out inputParse));
+
+            for(int i = 1; i <= inputParse; i++)
+            {
+                Console.WriteLine($"Counting: ...{i}");
+            }
+
+            input = null;
+
+            do
+            {
+                Console.WriteLine(input == null ? "Enter another Number" : "You entered an invaid number, please try again!");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out inputParse));
+
+            for (int i = inputParse; i >= 1; i--)
+            {
+                Console.WriteLine($"Counting: ...{i}");
+            }
+
+        }
+    }
+}
---
+++ Homework_02/Exercise2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Homework_02
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
